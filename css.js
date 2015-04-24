236c76df7b910c39e6701d946d05b3be5bb5f75d
Cosmic Web Services <andrewrodebaugh@cosmicsearch.org>
2015-04-24 17:10:00 -0400

    Update css.js

--- css.js
+++ css.js
@@ -3,7 +3,7 @@
 function cssjs(csscode) {
     
    var css = csscode;
-   var head = document.head || document.getElementsByTagName('head')[0]
+   var head = document.head || document.getElementsByTagName('head')[0];
    var style = document.createElement('style');
 
    style.type = 'text/css';
